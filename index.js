f3842259ce191c07695f87aa86afbb347cc56adf
Romy-Mukendi <70291492+Romy-Mukendi@users.noreply.github.com>
2025-10-01 06:18:59 -0600

    HTML, css and some javascipt

---
+++ index.js
@@ -0,0 +1,39 @@
+let wasClicked = false;
+
+$(".btn").on("click", function(){
+    const initBackgroundColor = $(this).css("background-color");
+    this.style.backgroundColor = "red";
+    setTimeout(() =>{
+       $(this).css("background-color", initBackgroundColor);
+    }, 200);
+    $("body").css("background-color", "red");
+    setTimeout(() =>{
+        $("body").css("background-color", "#011F3F") 
+    }, 200)
+
+    const wrongInputAudio = new Audio("/sounds/wrong.mp3");
+    wrongInputAudio.play();
+
+
+    let allButtonClasses = $(".btn").attr("class");
+    let classesArray = allButtonClasses.split(" ");
+    let buttonClass = classesArray[1];
+    let buttonAudio = new Audio("/sounds/" + buttonClass + ".mp3");
+    buttonAudio.play();
+});
+
+
+let randomNum = Math.floor(Math.random() * 4) + 1;
+let count = 1;
+
+//$("button").on("click", function(event){
+   // const randomButton = parseInt(event.target.class);
+    //if (randomNum != randomButton){
+      //  $("body").css("background-color", "red");
+      //  setTimeout(function() {
+    //$("body").css("background-color", "#011F3F") 
+//}, 200)
+
+   // }
+//})
+ 
